bdc80759a928133d13983b2ebd245ecc396145f2
Zhivko Kocev <zikekocev57@gmail.com>
2024-05-18 11:28:10 +0200

    Added my project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
